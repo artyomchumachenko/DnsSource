09ddb521821c0a3289b187560dfdb703142a78a1
artyomchumachenko <artyomchumachenko@mail.ru>
2022-10-08 17:46:15 +0300

    Creating project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module ru.mai.coursework.dns.dnssource {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires org.hibernate.orm.core;
+
+    opens ru.mai.coursework.dns.dnssource to javafx.fxml;
+    exports ru.mai.coursework.dns.dnssource;
+}
---
+++ src/main/java/ru/mai/coursework/dns/dnssource/HelloController.java
@@ -0,0 +1,14 @@
+package ru.mai.coursework.dns.dnssource;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
